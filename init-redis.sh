7af060236ae3834deae24695d876dd9c0b47a319
Sadeq N. Yazdi <sadeq-n-yazdi@users.noreply.github.com>
2024-09-28 12:35:25 +0100

    fix initi redis script

--- init-redis.sh
+++ init-redis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WORKDIR=$(realpath)
-
+WORKDIR=$(dirname $(realpath $0))
+ls -lh "${WORKDIR}/redis.conf"
  docker container rm huma-redis 2>/dev/null && echo "old redis container stopped"
  docker run -d --rm --name huma-redis  --volume="${WORKDIR}/redis.conf:/redis.conf:ro" -p 6379:6379 -d redis:alpine redis-server  /redis.conf && \
  sleep 1 && \
